1896f6e60aabefa52eb483158b91cc2be21da1f7
babst3alth <babalolaoluwapelumi9@gmail.com>
2024-01-06 16:08:27 +0100

    made only one article can be opened at a time

--- 07-questions/app.js
+++ 07-questions/app.js
@@ -7,6 +7,12 @@ questions.forEach((question) => {
     const btn = question.querySelector('.question-btn')
     // console.log(btn)
     btn.addEventListener('click', () => {
+        questions.forEach((item) => {
+            if (item !== question) {
+                item.classList.remove('show-text')
+            }
+        })
+
         question.classList.toggle('show-text')
     })
 })
