43b6d0d7a27aae4119cd0135092dcb87c200e04b
babst3alth <babalolaoluwapelumi9@gmail.com>
2024-01-06 15:45:15 +0100

    started an alternative method

--- 07-questions/app.js
+++ 07-questions/app.js
@@ -1,11 +1,26 @@
 //using selectors inside the element
+
+const questions = document.querySelectorAll('.question')
+
+
+
+
+
+
+
+
+
+
+
+
+
 // traversing the dom
 
-const btns = document.querySelectorAll('.question-btn')
-
-btns.forEach((btn) => {
-    btn.addEventListener('click', (e) => {
-        const question = (e.currentTarget.parentElement.parentElement)
-        question.classList.toggle('show-text')
-    })
-})
+// const btns = document.querySelectorAll('.question-btn')
+//
+// btns.forEach((btn) => {
+//     btn.addEventListener('click', (e) => {
+//         const question = (e.currentTarget.parentElement.parentElement)
+//         question.classList.toggle('show-text')
+//     })
+// })
