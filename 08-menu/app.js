e0b806c036ed2657731cb6960ea2626437683ab7
babst3alth <babalolaoluwapelumi9@gmail.com>
2024-01-08 20:45:07 +0100

    added an object and started creating button in javascript

--- 08-menu/app.js
+++ 08-menu/app.js
@@ -71,6 +71,14 @@ const menu = [
     img: "./images/item-9.jpeg",
     desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
   },
+  {
+    id: 10,
+    title: "steak dinner",
+    category: "dinner",
+    price: 39.99,
+    img: "./images/item-10.jpeg",
+    desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
+  },
 ];
 
 const sectionCenter = document.querySelector('.section-center')
@@ -79,6 +87,9 @@ const filterBtns = document.querySelectorAll('.filter-btn')
 //load items
 window.addEventListener('DOMContentLoaded', () => {
   displayMenuItems(menu)
+  const categories = menu.reduce(() => {
+
+  },['all'])
 })
 
 //filter items
