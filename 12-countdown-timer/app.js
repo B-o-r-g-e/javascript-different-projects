01ae188fe63cb645e1920a9371d66575485cfce1
babst3alth <babalolaoluwapelumi9@gmail.com>
2024-01-10 09:31:24 +0100

    added year,hours & minutes

--- 12-countdown-timer/app.js
+++ 12-countdown-timer/app.js
@@ -27,5 +27,10 @@ const deadline = document.querySelector('.deadline')
 const items = document.querySelectorAll('.deadline-format h4')
 
 let futureDate = new Date(2024, 0, 10, 11, 30, 0)
+console.log(futureDate)
 
 const year = futureDate.getFullYear()
+const hours = futureDate.getHours()
+const minutes = futureDate.getMinutes()
+
+giveaway.textContent = `giveaway ends on ${year} ${hours}:`
