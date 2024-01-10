f011d52971df46a2e9f7170c21a97919c6d4f3a6
babst3alth <babalolaoluwapelumi9@gmail.com>
2024-01-10 11:49:06 +0100

    changed date format

--- 12-countdown-timer/app.js
+++ 12-countdown-timer/app.js
@@ -31,8 +31,8 @@ let tempYear = tempDate.getFullYear()
 let tempMonth = tempDate.getMonth()
 let tempDay = tempDate.getDate()
 
-let futureDate = new Date(2024, 0, 13, 11, 30, 0)
-// console.log(futureDate)
+// let futureDate = new Date(2024, 0, 13, 11, 30, 0)
+const futureDate = new Date(tempYear, tempMonth,tempDay + 3, 30, 0)
 
 const year = futureDate.getFullYear()
 const hours = futureDate.getHours()
