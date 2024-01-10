29e2c56dc3f8c4d00d3a10dd207344522ff07b9e
babst3alth <babalolaoluwapelumi9@gmail.com>
2024-01-10 10:57:46 +0100

    .

--- 12-countdown-timer/app.js
+++ 12-countdown-timer/app.js
@@ -26,7 +26,7 @@ const giveaway = document.querySelector('.giveaway')
 const deadline = document.querySelector('.deadline')
 const items = document.querySelectorAll('.deadline-format h4')
 
-let futureDate = new Date(2024, 0, 10, 11, 30, 0)
+let futureDate = new Date(2024, 0, 13, 11, 30, 0)
 // console.log(futureDate)
 
 const year = futureDate.getFullYear()
@@ -44,3 +44,30 @@ giveaway.textContent = `giveaway ends on ${weekday} ${date} ${month} ${year} ${h
 
 //future time in ms
 const futureTime = futureDate.getTime()
+// console.log(futureTime)
+
+function getRemainingTime(){
+  const today = new Date().getTime()
+  const t = futureTime - today
+
+  /*
+  * 1s = 1000ms
+  * 1min = 60s
+  * 1hr = 60mins
+  * 1day = 24hrs
+  */
+
+  //values in ms
+  const oneDay = 24 * 60 * 60 * 1000
+  const oneHour = 60 * 60 * 1000
+  const oneMinute = 60 * 1000
+
+  //calculate all values
+  let days = Math.floor(t/oneDay)
+  // days = Math.floor(days)
+  let hours = Math.floor((t % oneDay) / oneHour)
+
+
+}
+
+getRemainingTime()
