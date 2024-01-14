3078a92c048c5a0f518c1db4605218eedc49bfab
babst3alth <babalolaoluwapelumi9@gmail.com>
2024-01-14 08:52:27 +0100

    .

--- 15-slider/app.js
+++ 15-slider/app.js
@@ -9,12 +9,22 @@ slides.forEach( (slide, index) => {
 let counter = 0
 nextBtn.addEventListener('click', () => {
     counter++
+    carousel()
 })
 prevBtn.addEventListener('click', () => {
     counter--
+    carousel()
 })
 
 function carousel() {
+    //working with slides
+    if (counter === slides.length) {
+        counter = 0
+    }
+    if (counter < 0) {
+        counter = slides.length - 1
+    }
+
     slides.forEach((slide) => {
         slide.style.transform = `translateX(-${counter * 100}%)`
     })
