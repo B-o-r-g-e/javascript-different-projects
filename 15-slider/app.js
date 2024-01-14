4862c9d39bea631e9f54e8e431b360aeeeef2866
babst3alth <babalolaoluwapelumi9@gmail.com>
2024-01-14 08:39:31 +0100

    .

--- 15-slider/app.js
+++ 15-slider/app.js
@@ -2,7 +2,11 @@ const slides = document.querySelectorAll('.slide')
 const nextBtn = document.querySelector('.nextBtn')
 const prevBtn = document.querySelector('.prevBtn')
 
-slides.forEach(function (slide, index){
+slides.forEach( (slide, index) => {
     slide.style.left = `${index * 100}%`
 })
 
+let counter = 0
+nextBtn.addEventListener('click', () => {
+
+})
