68440a0909977c5ac48876e33fd3be4b9a4128c9
babst3alth <babalolaoluwapelumi9@gmail.com>
2024-01-15 23:47:38 +0100

    .

--- 16-counter/app.js
+++ 16-counter/app.js
@@ -2,27 +2,33 @@ let count;
 
 let value = document.querySelector('.value')
 const btns = document.querySelectorAll('.btn')
-let firstCounter = document.querySelector('.first-counter')
-let secondCounter = document.querySelector('.second-counter')
+const containers = document.querySelectorAll('.container')
 
 count = value.textContent
 
-btns.forEach((btn) => {
-    btn.addEventListener('click', (e) => {
-       const current = e.currentTarget.classList
-        console.log(current)
-        if (current.contains('decrease')) {
-            count--
-        } else if (current.contains('reset')) {
-            count = 0
-        } else if (current.contains('increase')) {
-            count++
-        }
-
-        if (count < 0) {
-            count = 0
-        }
-
-        value.textContent = count
-    })
+containers.forEach(() => {
+    buttons();
 })
+
+
+function buttons() {
+    btns.forEach((btn) => {
+        btn.addEventListener('click', (e) => {
+            const current = e.currentTarget.classList
+            console.log(current)
+            if (current.contains('decrease')) {
+                count--
+            } else if (current.contains('reset')) {
+                count = 0
+            } else if (current.contains('increase')) {
+                count++
+            }
+
+            if (count < 0) {
+                count = 0
+            }
+
+            value.textContent = count
+        })
+    })
+}
