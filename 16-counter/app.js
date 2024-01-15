3d012f6704fa7a6cae3b7f25b673700f24555314
babst3alth <babalolaoluwapelumi9@gmail.com>
2024-01-15 10:26:08 +0100

    .

--- 16-counter/app.js
+++ 16-counter/app.js
@@ -1,5 +1,28 @@
-//Buttons & DOM
-const decrease = document.querySelector('.decrease')
-const reset = document.querySelector('.reset')
-const increase = document.querySelector('.increase')
-const value = document.querySelector('.value')
+let count;
+
+let value = document.querySelector('.value')
+const btns = document.querySelectorAll('.btn')
+let firstCounter = document.querySelector('.first-counter')
+let secondCounter = document.querySelector('.second-counter')
+
+count = value.textContent
+
+btns.forEach((btn) => {
+    btn.addEventListener('click', (e) => {
+       const current = e.currentTarget.classList.parent
+        console.log(current)
+        // if (current.contains('decrease')) {
+        //     count--
+        // } else if (current.contains('reset')) {
+        //     count = 0
+        // } else if (current.contains('increase')) {
+        //     count++
+        // }
+        //
+        // if (count < 0) {
+        //     count = 0
+        // }
+        //
+        // value.textContent = count
+    })
+})
