692dcd8b11f5685f3ecfc9886b2cfb2724304014
babst3alth <babalolaoluwapelumi9@gmail.com>
2024-01-15 08:56:10 +0100

    DOM value

--- 16-counter/app.js
+++ 16-counter/app.js
@@ -1,4 +1,5 @@
-//Buttons
+//Buttons & DOM
 const decrease = document.querySelector('.decrease')
 const reset = document.querySelector('.reset')
 const increase = document.querySelector('.increase')
+const value = document.querySelector('.value')
